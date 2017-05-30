59627f1481aefe1bb88d206c3d3cae51ff0ec080
juan <chavezvasquezjuan@gmail.com>
2017-05-29 22:06:44 -0500

    decorators

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    url(r'^home/', views.home),
+    url(r'^home/', login_required(views.home), name = 'home'),
 ]
