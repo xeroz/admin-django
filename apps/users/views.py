5b4268ab3a2be4cd4f129a296b82073b42630e6c
juan <chavezvasquezjuan@gmail.com>
2017-06-04 01:11:32 -0500

    def create

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, HttpResponseRedirect
+from apps.users.forms import Registrationform
 
 # Create your views here.
 def index(request):
-    return render(request, 'users/index.html', {})
+
+    data = {}
+    return render(request, 'users/index.html', data)
 
 def create(request):
+
+    if request.method == 'POST':
+        form = Registrationform(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/users/index')
+    else:
+        form = Registrationform()
+        data = {'form': form}
+        return render(request, 'users/create.html', data)
+
     return render(request, 'users/create.html', {})
