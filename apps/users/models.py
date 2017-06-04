4698500d3040ce53d85253b6bd1b06c7a8249c10
juan <chavezvasquezjuan@gmail.com>
2017-06-04 01:10:55 -0500

    model user

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    user        = models.OneToOneField(User)
+    description = models.CharField(max_length = 100, default='')
+    city        = models.CharField(max_length = 100, default = '')
+    website     = models.URLField(max_length = 100, default='')
+    phone       = models.IntegerField(default=0)
