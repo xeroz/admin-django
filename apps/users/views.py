1a5c2b81add3788401af756f2f4bc4a7d1ef6563
juan <chavezvasquezjuan@gmail.com>
2017-06-18 20:55:48 -0500

    crud users

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from apps.users.forms import Registrationform
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
 
-    data = {}
+    users = User.objects.all()
+    data = {
+        'users': users,
+    }
     return render(request, 'users/index.html', data)
 
 def create(request):
@@ -20,3 +24,35 @@ def create(request):
         return render(request, 'users/create.html', data)
 
     return render(request, 'users/create.html', {})
+
+def edit(request, id):
+
+    user = get_object_or_404(User, id = id)
+
+    if request.method == 'POST':
+        first_name = request.POST["first_name"]
+        last_name  = request.POST["last_name"]
+        username   = request.POST["username"]
+        email      = request.POST["email"]
+
+        user.first_name = first_name
+        user.last_name  = last_name
+        user.username   = username
+        user.email      = email
+
+        user.save()
+
+        return HttpResponseRedirect('/users/index')
+
+    data = {
+        'user': user,
+    }
+
+    return render(request, 'users/edit.html', data)
+
+def delete(render, id):
+
+    user = get_object_or_404(User, id = id)
+    user.delete()
+
+    return HttpResponseRedirect('/users/index')
