179ad39a734de781725c8b5efc292eb210cef473
juan <chavezvasquezjuan@gmail.com>
2017-07-02 01:33:59 -0500

    add filed competitions

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from apps.competitions.models import Competition
 
 # Create your models here.
 class Team(models.Model):
-    name       = models.CharField(max_length=50, default='')
-    foundation = models.DateField()
+    name        = models.CharField(max_length=50, default='')
+    foundation  = models.DateField()
+    competition = models.ManyToManyField(Competition)
 
     def __str__(self):
         return self.name
