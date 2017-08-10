06ae3daae37a4f0fb56df67cecdfee40b099a1a4
staff-digital <chavezvasquezjuan@gmail.com>
2017-08-10 11:39:41 -0500

    settings

--- admin/settings.py
+++ admin/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'geoposition',
-    # 'wkhtmltopdf',
+    'wkhtmltopdf',
     'django_countries',
     'apps.home',
     'apps.users',
